5631c8689ad4148a445da62cdc5df450ed384dc6
raflydr <raflydr48@gmail.com>
2023-03-25 00:47:16 +0700

    first

---
+++ resources/views/layouts/tamplate.blade.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- style="background-color: #396844;" -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>">
+</head>
+
+<body>
+    <nav class="navbar shadow-lg fixed-top navbar-expand-lg navbar-light bg-light">
+
+        <div class="container-fluid"><img src="images/logo-atas.png" width="20%"></img>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown" style="margin-left: 2rem;">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Profil 
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        <li class="nav-item" style="margin-left: 0.5rem;">
+                            <a class="nav-link active" aria-current="page" href="#">Eselon 1</a>
+                        </li>
+                        <li class="nav-item dropdown" style="margin-left: 0.5rem;">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Informasi Public
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                            <li class="nav-item" style="margin-left: 0.5rem;">
+                                <a class="nav-link" href="#">Mall Pelayanan Kemenkeu</a>
+                            </li>
+                            <li class="nav-item dropdown" style="margin-left: 0.5rem;">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Transformasi Kemenkeu
+                        </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
+
+    <h2 style="margin: 8% 0% 0 8%;">Profil Pejabat</h2>
+
+    <div class="row" style="margin-top: 3%;">
+        <div class="col" style="margin-left: 6%;">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/srimulyani.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Sri Mulyani Indrawati</h3>
+                            <h5 class="card-title">Menteri Keuangan</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="srimul.html" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col" style="margin-right: 3%;">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/suahasil.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Suahasil Nazara</h3>
+                            <h5 class="card-title">Wakil Menteri Keuangan</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col" style="margin-left: 6%;">
+            <div class="card mb-3" style="max-width: 540px; margin-top: 3%;">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/herupambudi.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Heru Pambudi</h3>
+                            <h5 class="card-title">Sekretaris Jendral</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col" style="margin-right: 3%;">
+            <div class="card mb-3" style="max-width: 540px; margin-top: 3%; ">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/isa.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Isa Rachmatarwata</h3>
+                            <h5 class="card-title">Direktur Jendral Anggaran</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col" style="margin-left: 6%;">
+            <div class="card mb-3" style="max-width: 540px; margin-top: 3%;">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/suryo.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Suryo Utomo</h3>
+                            <h5 class="card-title">Direktur Jendral Pajak</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="col" style="margin-right: 3%;">
+            <div class="card mb-3" style="max-width: 540px; margin-top: 3%;">
+                <div class="row g-1">
+                    <div class="col-md-4">
+                        <img src="images/askolani.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Askolani</h3>
+                            <h5 class="card-title">Direktur Jendral Bea Cukai</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <a href="#" class="btn btn-primary">Lihat Detail</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <nav aria-label="Page navigation example" style="margin-left: 40%;">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+
+    <!-- Footer -->
+
+    <footer class="text-center text-lg-start text-muted" style="background-color: #01347c" ">
+        <!-- Section: Social media -->
+        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom " style="background-color: #02275d; ">
+            <!-- Left -->
+            <div class="me-5 d-none d-lg-block " style="margin-left: 38%; ">
+                <span href="# " class="btn " style="color: white; ">Peta Situs </span>
+                <span href="# " class="btn " style="color: white; ">Hubungi Kami</span>
+                <span href="# " class="btn " style="color: white; ">Prasyarat</span>
+            </div>
+            <!-- Left -->
+
+            <!-- Right -->
+            <div>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fa-brands fa-facebook-f "></i>
+                </a>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fa-brands fa-twitter "></i>
+                </a>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fab fa-google "></i>
+                </a>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fab fa-instagram "></i>
+                </a>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fab fa-linkedin "></i>
+                </a>
+                <a href=" " class="me-4 text-reset ">
+                    <i class="fab fa-github "></i>
+                </a>
+            </div>
+            <!-- Right -->
+        </section>
+        <!-- Section: Social media -->
+
+        <!-- Section: Links  -->
+        <section class="footer-bawah ">
+            <div class="container text-center text-md-start mt-5 ">
+                <!-- Grid row -->
+                <div class="row mt-3 ">
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-1 col-xl-1 mx-auto mb-1 ">
+                        <img src="images/logo-kementerian.png " width="70% " style="margin: 0 0 3rem 3rem; ">
+                        <!-- Content -->
+                    </div>
+
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 ">
+                        <!-- Content -->
+                        <h6 class="text-uppercase fw-bold mb-4 ">
+                            <p> </p>
+                            <i class="fas fa-gem "></i>Kementerian Keuangan Republik Indonesia
+                        </h6>
+                        <p>
+                            Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-4 mx-auto mb-4 " style="padding-left: 10%; ">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-4 ">
+                            Artikel Gpr
+                        </h6>
+                        <p>
+                            <a>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore maxime voluptatibus optio at odit fugit aspernatur provident repellendus laborum libero soluta repudiandae voluptatem unde similique consequuntur enim eos, impedit beatae.</a>
+                        </p>
+                        
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 ">
+                        <!-- Links -->
+                        <h6 class="text-uppercase fw-bold mb-3 ">Ikuti kami di kanal :</h6>
+                        <div class="row ">
+                            <div class="col-sm-3 btn "><img src="images/facebook-app-symbol.png " width="60% "></div>
+                            <div class="col-sm-3 btn "><a href="https://instagram.com/rahmadhanirafly "><img src="images/instagram.png " width="60% "></a></div>
+                            <div class="col-sm-3 btn "><img src="images/twitter.png " width="60% " ></div>
+                        </div>
+                        <div class="row " style="padding-top: 1rem; ">
+                            <div class="col-sm-3 btn "> <a href="https://www.youtube.com/@raflydivanandra7968 "><img src="images/youtube.png " width="60% "></a></div>
+                            <div class="col-sm-3 btn "><img src="images/tiktok.png " width="60% "></div>
+                            <div class="col-sm-3 btn "><img src="images/linkedin.png " width="60% " ></div>
+                          </div>
+                    </div>
+                    <!-- Grid column -->
+                </div>
+                <!-- Grid row -->
+            </div>
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        
+    </footer>
+
+    
+    <!-- Footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js " integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM " crossorigin="anonymous "></script>
+</body>
+
+</html>
